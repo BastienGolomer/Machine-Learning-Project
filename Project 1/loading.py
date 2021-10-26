efb2a301c8af6bb6c18526834bc6c0b09bdd71dc
Arthur Brousse <arthur.brousse@epfl.ch>
2021-10-26 10:46:54 +0200

    added the writecsv function

--- Project 1/loading.py
+++ Project 1/loading.py
@@ -8,8 +8,20 @@ def load_csv_data(path_data):
     
     input_data = x[:, 2:]
     
-    # Convert class labels from strings to binary (-1,1)
+    # Y has class labels, we convert them to 1 or -1 to be able to manipulate them in an easier way.
     yb = np.ones(len(y))
     yb[np.where(y == 'b')] = 0
     
     return yb, input_data, ids
+
+def write_csv(ids, predictions, name_csv):
+### Creates an output submission in a csv file to AICrowd
+'''ids : id of the event of the associated prediction
+   predictions : class label of the variable y 
+   name_csv : name of the csv file in which we write our results '''
+    with open(name, newline = ' ') as csvfile:
+        names = ["Id", "Prediction"]
+        writer = csv.DictWriter(csvfile, delimiter = ' ', fieldnames = names)
+        writer.writeheader()
+        for x, y in ids, prediction:
+            writer.writerow({"Id" : int(x), "Prediction" : int(predictions)})
