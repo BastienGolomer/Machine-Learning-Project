c90a41a9122699c7b2dc81538179dbd6b37aa14a
Bastien Le Lan <bastien.lelan@epfl.ch>
2021-10-27 23:37:55 +0200

    ca merde avec test.csv psk en fonction des PRI_jet_mes_couilles ya pas le meme nombre enlevés

--- Project 1/K_fold.py
+++ Project 1/K_fold.py
@@ -1,10 +1,10 @@
 import numpy as np
 import loading as ld
 from implementations import *
+from loss_functions import *
 
 def K_fold_split(K, train, y):
     len_valid=len(train)/K
-    #print(y)
     pieces=np.array_split(train, K, axis=0)
     ypieces=np.array_split(y, K)
     X_train=[]
@@ -12,38 +12,32 @@ def K_fold_split(K, train, y):
     X_validate=[]
     Y_validate=[]
     for i in range (0,K):
-        np.append(X_validate,pieces[i])
-        np.append(Y_validate,ypieces[i])
+        X_validate.append(pieces[i])
+        Y_validate.append(ypieces[i])
         temp=np.zeros(len(train[0,:]))
-        #print (len(temp))
         ytemp=np.zeros(1)
         for j in range (0,K):
             if j!=i:
                 temp=np.vstack((temp,pieces[j]))
                 ytemp=np.concatenate((ytemp,ypieces[j]), axis= None)
-        #print(temp[1,:])
-        #print(X_train)
         X_train.append(temp[1:,:])
-        Y_train.append(ytemp[1:])    
+        Y_train.append(ytemp[1:]) 
     return X_train,Y_train,X_validate,Y_validate
 
 def K_fold(y,X):
-    #y, X, labels = ld.load_csv_data('./train.csv')
     trainx,trainy,valx,valy=K_fold_split(8,X,y)
     w=[]
     losses=[]
+    #valx=np.array(valx)
     for i in range (0,8):
-        temp=least_squares(trainy[i],trainx[i])
-        #print(trainx)
+        temp=ridge_regression(trainy[i],trainx[i],0.4)
         w.append(temp[0])
-        losses.append(temp[1])
+        losses.append(mse(valy[i],valx[i],temp[0]))
     loss=1/8*sum(losses)
-    #print ((w[0]))
     w=np.array(w)
     wtemp=[]
     for i in range(0,len(w[0,:])):
         wtemp.append(1/8*sum(w[:,i]))
-    print(wtemp)
-    return wtemp, loss
+    return [wtemp, loss]
 
     
--- Project 1/spli_PRI_jet_num.py
+++ Project 1/spli_PRI_jet_num.py
@@ -8,40 +8,59 @@ import loading as ld
 
 def split():
     y,X,ids=ld.load_csv_data("./train.csv")
-    X=np.c_(y,X)
+    test_y,test_X,test_ids=ld.load_csv_data("./test.csv")
+    X=np.c_[y,X]
+    a=np.linspace(1,len(test_X[:,1]),len(test_X[:,1]))
+    test_x=np.c_[a,test_X]
     # Data processing : getting rid of the columns which have "-999" values
     new_X = X.copy()
-    #new_X = np.delete(new_X, np.where(new_X == -999)[1], axis=1)
-    print(X)
     # the variable PRI_jet_num seems quite central, as many other variables depend on it
     # here the data is separated following this fact
-    PRI_jet_num_0 = new_X[:][new_X[:,24] == 0]
+    #train
+    new_PRI_jet_num_0 = splity_split_yo(new_X,0)
+    new_PRI_jet_num_1 = splity_split_yo(new_X,1)
+    new_PRI_jet_num_2 = splity_split_yo(new_X,2)
+    new_PRI_jet_num_3 = splity_split_yo(new_X,3)
+    
+    #print(new_PRI_jet_num_0[:,0].shape, new_PRI_jet_num_0[:,1:].shape)
+    PRI0=K_fold(new_PRI_jet_num_0[:,0],new_PRI_jet_num_0[:,1:])
+    PRI1=K_fold(new_PRI_jet_num_1[:,0],new_PRI_jet_num_1[:,1:])
+    PRI2=K_fold(new_PRI_jet_num_2[:,0],new_PRI_jet_num_2[:,1:])
+    PRI3=K_fold(new_PRI_jet_num_3[:,0],new_PRI_jet_num_3[:,1:])
+    #split for test
+    ychap0=traitements_test_set(0,test_X,PRI0[0])
+    ychap1=traitements_test_set(1,test_X,PRI0[0])
+    ychap2=traitements_test_set(2,test_X,PRI0[0])
+    ychap3=traitements_test_set(3,test_X,PRI0[0])
+    
+    yhat=[ychap0]
+    yhat.extend(ychap1)
+    yhat.extend(ychap2)
+    yhat.extend(ychap3)
+    #print(PRI0)
+    loss=1/4*(PRI0[1]+PRI1[1]+PRI2[1]+PRI3[1])
+    #permet d'avoir la moyenne des arguments mais pas reelement utile car il n'y a jamais le meme nombre d'arg
+    w=[PRI0[0]]
+    w.append(PRI1[0])
+    w.append(PRI2[0])
+    w.append(PRI3[0])
+    wtemp=[]
+    for i in range(0,len(w[0])):
+        wtemp.append(1/4*(w[0][i]+w[1][i]+w[2][i]+w[3][i]))
+    print (loss)
+    return wtemp,loss
+
+
+def traitements_test_set(i, test_X,w):
+    PRI_test = splity_split_yo(test_X,i)
+    wtest=[1]
+    wtest.extend(w)
+    yshapoaud=PRI_test.dot(wtest)
+    yshapoaud=np.c_(PRI_test[:,0],yshapoaud)
+    return yshapoaud
+def splity_split_yo(new_X,i):
+    PRI_jet_num_0 = new_X[:][new_X[:,24] == i]
     new_PRI_jet_num_0 = PRI_jet_num_0.copy()
     new_PRI_jet_num_0 = np.delete(new_PRI_jet_num_0, np.where(new_PRI_jet_num_0 == -999)[1], axis=1)
-    print(new_PRI_jet_num_0)
-    PRI0=K_fold(new_PRI_jet_num_0[:,1],new_PRI_jet_num_0[:,2:])
-    
-    PRI_jet_num_1 = new_X[1:][new_X[1:,25] == 1]
-    new_PRI_jet_num_1 = PRI_jet_num_1.copy()
-    new_PRI_jet_num_1 = np.delete(new_PRI_jet_num_1, np.where(new_PRI_jet_num_1 == -999)[1], axis=1)
-    PRI1=K_fold(new_PRI_jet_num_1[:,1],new_PRI_jet_num_1[:,2:])
-    
-    PRI_jet_num_2 = new_X[1:][new_X[1:,25] == 2]
-    new_PRI_jet_num_2 = PRI_jet_num_2.copy()
-    new_PRI_jet_num_2 = np.delete(new_PRI_jet_num_2, np.where(new_PRI_jet_num_2 == -999)[1], axis=1)
-    PRI2=K_fold(new_PRI_jet_num_2[:,1],new_PRI_jet_num_2[:,2:])
-    
-    PRI_jet_num_3 = new_X[1:][new_X[1:,25] == 3]
-    new_PRI_jet_num_3 = PRI_jet_num_3.copy()
-    new_PRI_jet_num_3 = np.delete(new_PRI_jet_num_3, np.where(new_PRI_jet_num_3 == -999)[1], axis=1)
-    PRI3=K_fold(new_PRI_jet_num_3[:,1],new_PRI_jet_num_3[:,2:])
-    
-    loss=1/8*sum(PRI0[1]+PRI1[1]+PRI2[1]+PRI3[1])
-    #print ((w[0]))
-    w=np.array(PRI0[0],PRI1[0],PRI2[0],PRI3[0])
-    wtemp=[]
-    for i in range(0,len(w[0,:])):
-        wtemp.append(1/8*sum(w[:,i]))
-    print(wtemp, loss)
-    return wtemp,loss
+    return new_PRI_jet_num_0
 split()
