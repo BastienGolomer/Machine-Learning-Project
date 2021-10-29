ec85eccd71d5cc7135b0f5c53ef1f2ce0f8c0aab
Bastien Golomer <bastien.golomer@lapste.net>
2021-10-29 20:51:23 +0200

    minor updates

--- Project 1/K_fold.py
+++ Project 1/K_fold.py
@@ -3,9 +3,9 @@ import loading as ld
 from implementations import *
 from loss_functions import *
 
-def K_fold_split(K, train, y):
-    len_valid=len(train)/K
-    pieces=np.array_split(train, K, axis=0)
+def K_fold_split(X, y, K):
+    len_valid = len(X)/K
+    pieces=np.array_split(X, K, axis=0)
     ypieces=np.array_split(y, K)
     X_train=[]
     Y_train=[]
@@ -14,7 +14,7 @@ def K_fold_split(K, train, y):
     for i in range (0,K):
         X_validate.append(pieces[i])
         Y_validate.append(ypieces[i])
-        temp=np.zeros(len(train[0,:]))
+        temp=np.zeros(len(X[0,:]))
         ytemp=np.zeros(1)
         for j in range (0,K):
             if j!=i:
@@ -24,21 +24,21 @@ def K_fold_split(K, train, y):
         Y_train.append(ytemp[1:]) 
     return X_train,Y_train,X_validate,Y_validate
 
-def K_fold(y,X):
-    trainx,trainy,valx,valy=K_fold_split(8,X,y)
+def K_fold(X, y, K = 8):
+    trainx,trainy,valx,valy=K_fold_split(X, y, K)
     w=[]
     losses=[]
     #valx=np.array(valx)
-    for i in range (0,8):
-        temp=ridge_regression(trainy[i],trainx[i],0.3)
+    for i in range (0,K):
+        temp=least_squares(trainy[i],trainx[i])
+
         w.append(temp[0])
         losses.append(mse(valy[i],valx[i],temp[0]))
-    loss=1/8*sum(losses)
+    loss=1/K*sum(losses)
     w=np.array(w)
     wtemp=[]
     for i in range(0,len(w[0,:])):
-        wtemp.append(1/8*sum(w[:,i]))
-    print(len(wtemp))
+        wtemp.append(1/K*sum(w[:,i]))
     return [wtemp, loss]
 
     
