34abeb94357a11ec40c88b9514a5a6f8e18bbee5
Bastien Golomer <bastien.golomer@lapste.net>
2021-10-30 13:05:40 +0200

    update commit

--- Project 1/confusion_matrix.py
+++ Project 1/confusion_matrix.py
@@ -6,10 +6,8 @@ def compute_confusion_matrix(true_values, predicted_values):
 
   K = len(np.unique(true_values)) # Number of classes 
   result = np.zeros((K, K))
-  print(true_values)
   predicted_values[predicted_values < 0] = 0
   predicted_values[predicted_values > 0] = 1
-  print(predicted_values)
 
   for i in range(len(true_values)):
     result[int(true_values[i])][int(predicted_values[i])] += 1
